0fa4d19a75ca122943f3204fd22b54cd08c1f0de
LucasMorenoArg <lucas.moreno.ragni@mi.unc.edu.ar>
2024-05-07 09:32:29 -0300

    Primer commit

---
+++ src/main/java/com/ecommerceuserapi/service/UsuarioService.java
@@ -0,0 +1,43 @@
+package com.ecommerceuserapi.service;
+
+import com.ecommerceuserapi.entities.Usuario;
+import com.ecommerceuserapi.repository.UsuarioRepo;
+import interfaces.Prueba;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepo usuarioRepo;
+
+    public Iterable<Usuario> getAll() {
+        return usuarioRepo.findAll();
+    }
+
+    public Optional<Usuario> byId(int id) {
+        return usuarioRepo.findById(id);
+    }
+
+    public boolean save(Usuario usuario) {
+        usuarioRepo.save(usuario);
+        return true;
+    }
+
+    public String deleteById(int id) throws Exception {
+        try {
+            if (byId(id).isPresent()) {
+                Usuario usuario = byId(id).get();
+                usuarioRepo.delete(usuario);
+                return "El registro se eliminó correctamente";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
